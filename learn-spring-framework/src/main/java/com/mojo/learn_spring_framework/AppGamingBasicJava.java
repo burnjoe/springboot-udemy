0ab0c588585ebf0e6ac022688cb4e747722d815e
burnjoe <sabanajholo@gmail.com>
2024-08-19 07:43:50 +0800

    added GamingConsole implementation

--- learn-spring-framework/src/main/java/com/mojo/learn_spring_framework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/mojo/learn_spring_framework/AppGamingBasicJava.java
@@ -2,18 +2,21 @@ package com.mojo.learn_spring_framework;
 
 import com.mojo.learn_spring_framework.game.GameRunner;
 import com.mojo.learn_spring_framework.game.MarioGame;
+import com.mojo.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     
     public static void main(String[] args) {
     
         var marioGame = new MarioGame();
-        var gameRunner = new GameRunner(marioGame);
-        gameRunner.run();
+        var superContraGame = new SuperContraGame();
         
-        // Unable to pass the superContraGame to GameRunner() since it's tightly coupled to MarioGame`
-        // var superContraGame = new SuperContraGame();
-        // var gameRunner = new GameRunner(superContraGame);
-        // gameRunner.run();
+        // Runs MarioGame
+        var gameRunner1 = new GameRunner(marioGame);
+        gameRunner1.run();
+        
+        // RunsSuperContraGame
+        var gameRunner2 = new GameRunner(superContraGame);
+        gameRunner2.run();
     }
 }
--- learn-spring-framework/src/main/java/com/mojo/learn_spring_framework/game/SuperContraGame.java
+++ learn-spring-framework/src/main/java/com/mojo/learn_spring_framework/game/SuperContraGame.java
@@ -1,15 +1,15 @@
 package com.mojo.learn_spring_framework.game;
 
-public class MarioGame implements GamingConsole {
+public class SuperContraGame implements GamingConsole {
 
     @Override
     public void up() {
-        System.out.println("Jump");
+        System.out.println("Up");
     }
 
     @Override
     public void down() {
-        System.out.println("Go into a hole");
+        System.out.println("Sit down");
     }
 
     @Override
@@ -19,6 +19,6 @@ public class MarioGame implements GamingConsole {
 
     @Override
     public void right() {
-        System.out.println("Accelerate");
+        System.out.println("Shoot a bullet");
     }
 }
