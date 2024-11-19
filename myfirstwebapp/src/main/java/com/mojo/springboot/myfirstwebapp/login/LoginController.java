8c7f434e404f666edcb9d53d77feba81dd0b9896
burnjoe <sabanajholo@gmail.com>
2024-11-19 18:27:40 +0800

    implemented and learned logging

--- myfirstwebapp/src/main/java/com/mojo/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/mojo/springboot/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.mojo.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
     
+    // Logger instance for logging messages specific to this class
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     // http://localhost:8080/login?name=[value]
     @RequestMapping("login")
     // ModelMap allows us to pass the value of name to view (jsp) - similar to how 'props' works in frontend frameworks
@@ -15,6 +20,8 @@ public class LoginController {
     // To access name value in jsp: ${name}
     public String goToLogin(@RequestParam String name, ModelMap model) {
         model.put("name", name);
+        // Logs to console at debug level (Configure in application.properties)
+        logger.debug("Request param is {}", name);
         return "login";
     }
 }
