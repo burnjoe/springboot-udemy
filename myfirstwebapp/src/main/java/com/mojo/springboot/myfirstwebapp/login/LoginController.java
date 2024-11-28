3c45c23d17578eb121564f5192eb7c91fa43d5ad
mojo <sabanajholo@gmail.com>
2024-11-28 20:22:25 +0800

    implemented hard coded authentication service + error message in jsp

--- myfirstwebapp/src/main/java/com/mojo/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/mojo/springboot/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,6 @@
 package com.mojo.springboot.myfirstwebapp.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
 
     // http://localhost:8080/login | method = GET
     // Without specifying the method = RequestMethod.GET both GET and POST (form submit) is handled by /login
@@ -22,8 +27,12 @@ public class LoginController {
     // Both name and password is fetched from the request payload
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
-        model.put("name", name);
-        model.put("password", password);
-        return "welcome";
+        // Authentication
+        if (authenticationService.authenticate(name, password)) {
+            model.put("name", name);
+            return "welcome";
+        }
+        model.put("errorMessage", "Invalid Credentials. Please Try Again.");
+        return "login";
     }
 }
