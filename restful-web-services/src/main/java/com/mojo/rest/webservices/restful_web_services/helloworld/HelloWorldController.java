178c4b4e33ccb45162b572993fe57b7d67a034b9
mojo <56223696+burnjoe@users.noreply.github.com>
2025-05-01 14:15:26 +0800

    implemented endpoint with path variable

--- restful-web-services/src/main/java/com/mojo/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/mojo/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.mojo.rest.webservices.restful_web_services.helloworld;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +28,15 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World!");
+    }
+
+    // Path Parameters
+    // /users/{id}/todos/{id} => /users/1/todos/101
+    // This method will be called when the URL matches /hello-world/path-variable/{name}
+    // The {name} part of the URL is a path variable, which will be passed to the method as a parameter
+    // The @PathVariable annotation tells Spring to bind the value of the path variable to the method parameter
+    @GetMapping("hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s!", name));
     }
 }
